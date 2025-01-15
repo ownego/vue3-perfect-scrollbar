7fd2912dbfed8cd242f0114b8664057aa9c19e6d
TinhPT <tinh.phantrong0612@gmail.com>
2025-01-15 17:39:57 +0700

    Add style entry for style export, track dist directory

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+import { default as PerfectScrollbar } from './PerfectScrollbar.vue';
+import { PerfectScrollbarPluginType, PerfectScrollbarExpose } from './perfect-scrollbar';
+
+declare const PerfectScrollbarPlugin: PerfectScrollbarPluginType;
+export type { PerfectScrollbarExpose };
+export { PerfectScrollbar, PerfectScrollbarPlugin };
